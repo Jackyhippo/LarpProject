4b0b4eab82958e020f291be5efe128996757d469
hippo <jackyhung0211@gmail.com>
2025-02-03 16:33:39 +0800

    20250203更新

--- back/models/user.js
+++ back/models/user.js
@@ -1,21 +1,46 @@
 import { Schema, model } from 'mongoose'
 import validator from 'validator'
 import bcrypt from 'bcrypt'
+import UserRole from '../enums/UserRole'
 
 const schema = new Schema({
   account: {
     type: String,
-    required: [true, '使用者帳號必填'],
-    minlength: [4, '帳號太短'],
-    maxlength: [20, '帳號太長'],
+    required: [true, 'accountRequired'],
+    minlength: [4, 'accountTooShort'],
+    maxlength: [20, 'accountTooLong'],
+    // 必填
     unique: true,
     validate: {
       validator(value) {
-        // 使用 validator isAlphanumeric 判斷是否為英、數字
+        // 使用 validator 的 isAlphanumeric 判斷是否為英、數字
         return validator.isAlphanumeric(value)
       },
       message: 'accountInvalid',
     },
+  },
+  password: {
+    type: String,
+    required: [true, 'passwordRequired'],
+  },
+  email: {
+    type: String,
+    required: [true, 'emailRequired'],
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value)
+      },
+      message: 'emailInvalid',
+    },
+  },
+  // 保存登入的 jwt
+  tokens: {
+    type: [String],
+  },
+  role: {
+    type: Number,
+    default: UserRole.USER,
   },
 })
 
