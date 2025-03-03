c8fade75ad024028c00b38b0aafc526928c6f1be
hippo <jackyhung0211@gmail.com>
2025-03-03 16:37:59 +0800

    20250303進度

--- vuetify-project/src/layouts/footer.vue
+++ vuetify-project/src/layouts/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer class="text-white" height="auto" color="green">
+  <v-footer class="text-white" height="auto" color="black">
     <v-container>
       <v-row justify="center">
         <v-col class="text-center"> &copy; {{ new Date().getFullYear() }} 本是同根生 - All Rights Reserved </v-col>
